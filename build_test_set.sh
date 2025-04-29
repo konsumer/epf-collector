08a300876929b6bb41ae3780b22428b5c5964b94
David Konsumer <konsumer@jetboystudio.com>
2025-04-29 13:59:01 -0700

    test-set support

---
+++ build_test_set.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Some of the data has bad characters in lines. This will build a smaller test-set for those (so we can reverse it)
+
+mkdir -p data/test
+
+dir=$(pwd)
+
+for file in $(find data/epf -name '*.tbz'); do
+  pushd data/test
+  table=$(basename "${file}" .tbz)
+  echo "Creating test-set for ${table}"
+  bunzip2 -c "${dir}/${file}" | node "${dir}/lib/testset.js" > ${table} | bzip2 "${table}"
+  mv "${table}.bz2" "${table}.tbz"
+  popd
+done
